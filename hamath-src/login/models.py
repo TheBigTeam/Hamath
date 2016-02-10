406bb70310bf1186d95f35161ae75a2638f25e4c
Aidan Melen <aidan.melen94@gmail.com>
2016-02-10 09:01:08 -0700

    continue working on login

--- hamath-src/login/models.py
+++ hamath-src/login/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+class Login(models.Model):
+	email = models.CharField(max_length=50)
+	password = models.CharField(max_length=144)
+
+	def __unicode__(self):
+		return '/%s/' % self.name
+
+	#def get_absolute_url(self):
+		#return '/video/%s/' % self.id 
--- hamath-src/login/views.py
+++ hamath-src/login/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 from django.template.response import TemplateResponse
+from login.models import Login
 
 # Create your views here.
 
 def index(request):
 	#return HttpResponse("Hello Django")
-	return TemplateResponse(request, '/Users/aidanmelen/Developer/hammath-workspace/hamath-src/login/template/login.html', {})
+
+	data = Login.objects.all()
+
+	# returning raw data from the server
+	title = 'login page'
+
+	return TemplateResponse(request, '/Users/aidanmelen/Developer/hammath-workspace/hamath-src/login/template/login.html', { "data" : data })
